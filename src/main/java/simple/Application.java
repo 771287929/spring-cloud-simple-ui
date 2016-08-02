c58f0b46603bc9020d9548d8f55a2391eba04ff8
zhouwei <771287929@qq.com>
2016-08-02 16:11:20 +0800

    spring boot web 项目初始架构

---
+++ src/main/java/simple/Application.java
@@ -0,0 +1,31 @@
+package simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Controller
+@EnableWebMvc
+@EnableAutoConfiguration
+@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableHystrix
+public class Application extends WebMvcConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() {
+    	return "redirect:countries";
+    }
+}
+   
